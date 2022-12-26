3135d682840ac59bfd5e3fd9c02a9d1aebf0581c
ollypapa99 <muideenololade99@gmail.com>
2022-12-26 20:10:15 +0000

    terraforms file update

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket"
+    bucket = "my-dev-tf-state-bucket"
     key = "main"
-    region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table"
+    region = "us-east-2"
+    dynamodb_table = "my-dynamodb-table"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-east-1" 
+       default     = "us-east-2" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "mySep22Key" 
+    default     =  "MyNewEc2Key" 
 }
 
 variable "instance_type" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0ee23bfc74a881de5" 
+    default     = "ami-04fa64c4b38e36384" 
 }
 variable "versioning" {
     type        = bool
